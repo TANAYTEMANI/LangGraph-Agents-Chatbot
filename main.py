0cf4a49ab0b7db794284c57ab7d74f293f6c4d3f
Tanay Temani <82306595+TANAYTEMANI@users.noreply.github.com>
2025-04-05 19:58:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,19 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, StateGraph, START
 from langgraph.prebuilt import tools_condition
 from utilities import create_tool_node_with_fallback
-from langchain_core.messages import AIMessage
 from state import State
-import os
-from agents import part_1_tools ,part_1_assistant_runnable, Assistant
+from agents import part_1_tools, part_1_assistant_runnable, Assistant
 from utilities import tracer
 
 
-
-#Logging
+# Logging
 import logging
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-#User Graph
+# User Graph
 builder = StateGraph(State)
 builder.add_node("user", Assistant(part_1_assistant_runnable, tracer))
 builder.add_node("tools", create_tool_node_with_fallback(part_1_tools))
@@ -29,8 +27,7 @@ builder.add_conditional_edges(
 builder.add_edge("tools", "user")
 
 memory = MemorySaver()
-user_graph = builder.compile(checkpointer = memory)
-
+user_graph = builder.compile(checkpointer=memory)
 
 
 def process_user_query(messages: str, thread_id: str, vector_store) -> dict:
@@ -45,21 +42,14 @@ def process_user_query(messages: str, thread_id: str, vector_store) -> dict:
         dict: A response object containing assistant message.
     """
     config = {
-        "configurable": {
-            "thread_id": thread_id,
-            "vector_store": vector_store
-        },
-        "callbacks":[tracer]
+        "configurable": {"thread_id": thread_id, "vector_store": vector_store},
+        "callbacks": [tracer],
     }
     print("Thread Id", thread_id)
-
-    # logger.info(f"User State: {user_state}")
-    # logger.info(f"Config: {config}")
 
     memory.get(config=config)
 
     result = user_graph.invoke({"messages": messages}, config=config)
-    # memory.save(config=config)
 
     logger.info(f"Result: {result}")
 
